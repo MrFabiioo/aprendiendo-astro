26c447d5fcdb7687290cda90468650cb51954f11
Fabio Ortega <fabiolin57@gmail.com>
2025-06-23 12:17:41 -0500

    arreglado el problema de authors/index.astro

--- src/config/site-config.ts
+++ src/config/site-config.ts
@@ -1,34 +1,4 @@
 export const title = "Fabio Blogs"
 
-export const siteConfig = [{
-  imgSrc: 'https://images.unsplash.com/photo-1515378960530-7c0da6231fb1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-  titlePost: "How to use sticky note for problem solving",
-  date:"On: 20 October 2019"
-},
-{
-   imgSrc: "https://images.unsplash.com/photo-1497032628192-86f99bcd76bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" ,
-   titlePost: "How to use sticky note for problem solving",
-   date:"On: 20 October 2019"
-},
-{
-   imgSrc: "https://images.unsplash.com/photo-1497032628192-86f99bcd76bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" ,
-   titlePost: "Morning routine to boost your mood",
-   date:"On: 25 November 2020"
-},
-{
-   imgSrc: "https://images.unsplash.com/photo-1530099486328-e021101a494a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1547&q=80" ,
-   titlePost: "All the features you want to know",
-   date:"On: 30 September 2020"
-},
-{
-   imgSrc: "https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1484&q=80" ,
-   titlePost: "Minimal workspace for your inspirations",
-   date:"On: 13 October 2019"
-},
-{
-   imgSrc: "https://images.unsplash.com/photo-1624996379697-f01d168b1a52?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" ,
-   titlePost: "What do you want to know about Blockchane",
-   date:"On: 20 October 2019"
-}
-];
+
 
